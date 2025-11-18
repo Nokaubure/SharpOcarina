9eb018bab53b8e02ea5183328137cc4c46b1e83a
Nokaubure <ivansoto.80@gmail.com>
2025-11-18 12:48:18 +0100

    Autoupdate enabled

--- PickBlenderVersion.Designer.cs
+++ PickBlenderVersion.Designer.cs
@@ -31,6 +31,7 @@
             this.Ok = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.BlenderVersionComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Ok
@@ -46,32 +47,42 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 67);
+            this.label1.Location = new System.Drawing.Point(12, 67);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.Size = new System.Drawing.Size(126, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Blender Version:";
+            this.label1.Text = "Installed Blender Version:";
             // 
             // BlenderVersionComboBox
             // 
             this.BlenderVersionComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.BlenderVersionComboBox.FormattingEnabled = true;
-            this.BlenderVersionComboBox.Location = new System.Drawing.Point(110, 64);
+            this.BlenderVersionComboBox.Location = new System.Drawing.Point(144, 64);
             this.BlenderVersionComboBox.Name = "BlenderVersionComboBox";
-            this.BlenderVersionComboBox.Size = new System.Drawing.Size(141, 21);
+            this.BlenderVersionComboBox.Size = new System.Drawing.Size(128, 21);
             this.BlenderVersionComboBox.TabIndex = 2;
             this.BlenderVersionComboBox.SelectionChangeCommitted += new System.EventHandler(this.SceneSettingComboBox_SelectionChangeCommitted);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Warning: You must choose a 3.0+ version!";
             // 
             // PickBlenderVersion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 207);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.BlenderVersionComboBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Ok);
             this.Name = "PickBlenderVersion";
-            this.Text = "Pick Installed Blender Version";
+            this.Text = "Pick Blender Version";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.PickBlenderVersion_FormClosed);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -83,5 +94,6 @@
         private System.Windows.Forms.Button Ok;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox BlenderVersionComboBox;
+        private System.Windows.Forms.Label label2;
     }
 }
